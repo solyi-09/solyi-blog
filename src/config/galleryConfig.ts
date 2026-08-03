84e762e8aa582ab6b37aef46d402f3c5dde2497b
liyiwen1013 <liyiwen1013@gmail.com>
2026-08-03 12:38:31 +0800

    相册

--- src/config/galleryConfig.ts
+++ src/config/galleryConfig.ts
@@ -16,24 +16,40 @@ export const galleryConfig: GalleryConfig = {
 		// passwordHint: 密码提示，设置后在输入密码错误时显示（可选，需配合password使用）
 		// 每添加一个数组项就相当于添加了一个相册，记得在 public/gallery/ 目录下创建对应的子目录并放入图片
 		{
-			id: "firefly-2026",
-			name: "可爱流萤",
-			description: "飞萤之火自无梦的长夜亮起，绽放在终竟的明天。",
-			location: "崩坏：星穹铁道",
-			date: "2026-01-01",
-			tags: ["崩坏星穹铁道", "流萤"],
+			id: "solyi-2026",
+			name: "solyi",
+			description: "",
+			location: "生活随拍",
+			date: "2026-08-01",
+			tags: ["solyi", "生活"],
 		},
 		{
-			id: "encrypted-test",
-			name: "加密相册示例",
-			description:
-				"这是一个加密相册的示例，设置了访问密码，只有输入正确的密码才能查看相册内容。",
-			location: "崩坏：星穹铁道",
-			date: "2026-02-01",
-			tags: ["加密相册", "示例"],
-			password: "123456",
-			passwordHint: "示例密码123456",
+			id: "stp-2026",
+			name: "薯条集合册",
+			description: "",
+			location: "莎头相册集",
+			date: "2026-08-01",
+			tags: ["solyi", "stp"],
 		},
+		{
+			id: "sky",
+			name: "光遇",
+			description: "光是遇见，就很美好",
+			location: "sky-光遇",
+			date: "2026-08-01",
+			tags: ["solyi", "光遇"],
+		},
+		// {
+		// 	id: "encrypted-test",
+		// 	name: "加密相册示例",
+		// 	description:
+		// 		"这是一个加密相册的示例，设置了访问密码，只有输入正确的密码才能查看相册内容。",
+		// 	location: "崩坏：星穹铁道",
+		// 	date: "2026-02-01",
+		// 	tags: ["加密相册", "示例"],
+		// 	password: "123456",
+		// 	passwordHint: "示例密码123456",
+		// },
 	],
 
 	// 瀑布流最小列宽(px)，浏览器根据容器宽度自动计算列数，默认 240
